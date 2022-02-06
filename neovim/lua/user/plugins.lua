18127757e25642eb1c1f52bb1e8ce1e52433d0e1
Mateusz Szymiec <mattszymiec@gmail.com>
2022-02-06 14:33:56 +0100

    neovim: Add Packer and plugins file

---
+++ neovim/lua/user/plugins.lua
@@ -0,0 +1,41 @@
+-- install packer
+local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    PACKER_BOOTSTRAP = vim.fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
+    print "Installing Packer..."
+    vim.cmd [[packadd packer.nvim]]
+end
+
+-- reload neovim after saving the plugins file
+vim.cmd [[
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+]]
+
+-- error handler
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+-- plugins
+return packer.startup(function(use)
+    use "wbthomason/packer.nvim"
+    use "nvim-lua/popup.nvim"
+    use "nvim-lua/plenary.nvim"
+
+    -- automatically set up your configuration after cloning packer.nvim
+    -- put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+        require("packer").sync()
+    end
+end)
