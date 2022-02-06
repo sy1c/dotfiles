c2372ba13d99d290f5f7840abd06561d709c5373
Mateusz Szymiec <mattszymiec@gmail.com>
2022-02-06 18:50:15 +0100

    Update Neovim configuration - Treesitter

---
+++ neovim/lua/user/treesitter.lua
@@ -0,0 +1,16 @@
+local configs = require("nvim-treesitter.configs")
+configs.setup {
+    ensure_installed = "maintained",
+    sync_install = false, 
+    ignore_install = { "" },
+  
+    highlight = {
+        enable = true,
+        disable = { "" },
+        additional_vim_regex_highlighting = true,
+    },
+
+    indent = { 
+        enable = true,
+    },
+}
