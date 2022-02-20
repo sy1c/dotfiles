80f4d51f9c5ac88f629bdc940ad74fb14d8e166c
Mateusz Szymiec <mattszymiec@gmail.com>
2022-02-20 12:10:49 +0100

    Update Neovim configuration - Telescope

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,7 +1,8 @@
 require "user.options"
 require "user.plugins"
-require "user.treesitter"
 require "user.cmp"
 require "user.lsp"
+require "user.treesitter"
+require "user.telescope"
 
 vim.cmd "colorscheme nord"
