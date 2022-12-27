33e602876897c158806f4a3be19a71c59e0803c1
Mateusz Szymiec <mattszymiec@gmail.com>
2022-12-27 20:44:47 +0100

    neovim: Fix config after v8.1 update

--- neovim/lua/user/treesitter.lua
+++ neovim/lua/user/treesitter.lua
@@ -1,9 +1,19 @@
-local configs = require('nvim-treesitter.configs')
-configs.setup {
-    ensure_installed = 'all',
+require'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = {'c', 'cpp', 'python', 'lua'},
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false, 
+  
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+    
+    -- List of parsers to ignore installing (for "all")
     ignore_install = { '' },
   
+    -- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
     highlight = {
         enable = true,
         disable = { '' },
