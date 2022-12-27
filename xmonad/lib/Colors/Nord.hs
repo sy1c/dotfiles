e937637e5b9b83bffc2b4d26920e37f37333ef0e
Mateusz Szymiec <mattszymiec@gmail.com>
2022-12-27 21:36:41 +0100

    Revert "neovim: Delete files from the branch"
    This reverts commit e92bca066292087d039c9c020716af0ba28f3c04.

---
+++ xmonad/lib/Colors/Nord.hs
@@ -0,0 +1,22 @@
+module Colors.Nord where
+
+import XMonad
+
+colorScheme = "nord"
+
+color01 = "#2E3440"
+color02 = "#3B4252"
+color03 = "#434C5E"
+color04 = "#4C566A"
+color05 = "#D8DEE9"
+color06 = "#E5E9F0"
+color07 = "#ECEFF4"
+color08 = "#8FBCBB"
+color09 = "#88C0D0"
+color10 = "#81A1C1"
+color11 = "#5E81AC"
+color12 = "#BF616A"
+color13 = "#D08770"
+color14 = "#EBCB8B"
+color15 = "#A3BE8C"
+color16 = "#B48EAD"
